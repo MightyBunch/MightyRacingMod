df875a6a4b17d634b37f717e9bc7877f6772657d
MightyBunch <timababaev@bk.ru>
2025-02-04 20:20:45 +0300

    Durability system added, some small config changes

--- src/main/java/com/mightyracing/config/MightyConfig.java
+++ src/main/java/com/mightyracing/config/MightyConfig.java
@@ -13,6 +13,16 @@ public class MightyConfig {
     private static final String CONFIG_PATH = "config/mightyracing.properties";
     private static final Properties properties = new Properties();
 
+    public static String AUTO_FINISH = "auto_finish";
+    public static String CHECKPOINT_PRECISION = "checkpoint_precision";
+    public static String BROADCAST_ONLY_TO_DRIVERS = "broadcast_only_to_drivers";
+    public static String DESTROY_VEHICLE = "destroy_vehicle";
+    public static String DEFAULT_MODIF = "default_modif";
+    public static String DEFAULT_POWER = "default_power";
+    public static String SPECIAL_MODIF = "special_modif";
+    public static String SPECIAL_POWER = "special_power";
+    public static String SPECIAL_BLOCKS = "special_blocks";
+
     public static void register() {
         File file = new File(CONFIG_PATH);
         if (file.exists()) {
@@ -36,11 +46,26 @@ public class MightyConfig {
     private static String getProvider(){
         String provider = "";
         provider += "# Setting this to false allows lapped drivers to complete all their laps, even after another driver finishes.\n";
-        provider += propertyReg("auto_finish",true) + "\n";
+        provider += propertyReg(AUTO_FINISH,true);
         provider += "# This setting determines how many checkpoints can be skipped for the next one to count. A value of 0 means all previous checkpoints must be passed.\n";
-        provider += propertyReg("checkpoint_precision",0,0,10) + "\n";
+        provider += propertyReg(CHECKPOINT_PRECISION,0,0,10);
         provider += "# This setting controls whether broadcast messages (e.g. new fastest lap or racestatus changes) are sent to all players or only drivers.\n";
-        provider += propertyReg("broadcast_only_to_drivers",true);
+        provider += propertyReg(BROADCAST_ONLY_TO_DRIVERS,true);
+        provider += "# ================DURABILITY SYSTEM================\n\n";
+        provider += "# This setting determines whether the driver's vehicle (if they have one) will be destroyed when durability reaches zero.\n";
+        provider += propertyReg(DESTROY_VEHICLE,true);
+        provider += "# This setting controls how much speed influences overall durability reduction.\n";
+        provider += propertyReg(DEFAULT_MODIF,100f,0f,1000f);
+        provider += "# This setting controls how linearly durability decreases relative to speed. A value of 1 means a linear relationship.\n";
+        provider += propertyReg(DEFAULT_POWER,0.8f,0f,10f);
+        provider += "# This setting controls how much speed influences overall durability reduction when the driver is on the blocks listed below.\n";
+        provider += propertyReg(SPECIAL_MODIF,2f,0f,1000f);
+        provider += "# This setting controls how linearly durability decreases relative to speed when the driver is on the blocks listed below. A value of 1 means a linear relationship.\n";
+        provider += propertyReg(SPECIAL_POWER,3f,0f,10f);
+        provider += "# This setting defines the blocks for the above settings.\n";
+        provider += propertyReg(SPECIAL_BLOCKS,"minecraft:ice, minecraft:packed_ice, minecraft:blue_ice, minecraft:air",new String[]{});
+
+
         while (provider.endsWith("\n")) {
             provider = provider.substring(0, provider.length() - 1);
         }
@@ -52,25 +77,25 @@ public class MightyConfig {
         String defComment = (acceptString.isEmpty() ? "" : "# Accepted values: " + acceptString + "\n") + "# Default: " + defaultValue + "\n";
         if (!properties.containsKey(key) || (accepted.length != 0 && !Arrays.asList(accepted).contains(properties.getProperty(key)))){
             properties.setProperty(key,defaultValue);
-            return defComment + key + "=" + defaultValue + "\n";
+            return defComment + key + "=" + defaultValue + "\n\n";
         }
-        return defComment + key + "=" + properties.getProperty(key) + "\n";
+        return defComment + key + "=" + properties.getProperty(key) + "\n\n";
     }
 
     private static String propertyReg(String key,Boolean defaultValue){
         String defComment = "# Default: " + defaultValue + "\n";
         if (!properties.containsKey(key) || (!Objects.equals(properties.getProperty(key), "true") && !Objects.equals(properties.getProperty(key), "false"))){
             properties.setProperty(key, String.valueOf(defaultValue));
-            return defComment + key + "=" + defaultValue + "\n";
+            return defComment + key + "=" + defaultValue + "\n\n";
         }
-        return defComment + key + "=" + properties.getProperty(key) + "\n";
+        return defComment + key + "=" + properties.getProperty(key) + "\n\n";
     }
 
     private static String propertyReg(String key,Integer defaultValue, Integer min, Integer max){
         String defComment = "# Min: " + min + "\n# Max: " + max + "\n# Default: " + defaultValue + "\n";
         if (!properties.containsKey(key)){
             properties.setProperty(key, String.valueOf(defaultValue));
-            return defComment + key + "=" + defaultValue + "\n";
+            return defComment + key + "=" + defaultValue + "\n\n";
         }
         Integer intValue = defaultValue;
         try {
@@ -82,14 +107,14 @@ public class MightyConfig {
         } catch (Exception ignored) {
             properties.setProperty(key, String.valueOf(defaultValue));
         }
-        return defComment + key + "=" + intValue + "\n";
+        return defComment + key + "=" + intValue + "\n\n";
     }
 
     private static String propertyReg(String key,Float defaultValue, Float min, Float max){
         String defComment = "# Min: " + min + "\n# Max: " + max + "\n# Default: " + defaultValue + "\n";
         if (!properties.containsKey(key)){
             properties.setProperty(key, String.valueOf(defaultValue));
-            return defComment + key + "=" + defaultValue + "\n";
+            return defComment + key + "=" + defaultValue + "\n\n";
         }
         Float floatValue = defaultValue;
         try {
@@ -101,7 +126,7 @@ public class MightyConfig {
         } catch (Exception ignored) {
             properties.setProperty(key, String.valueOf(defaultValue));
         }
-        return defComment + key + "=" + floatValue + "\n";
+        return defComment + key + "=" + floatValue + "\n\n";
     }
 
     public static String getString(String key){
--- src/main/java/com/mightyracing/events/MightyTick.java
+++ src/main/java/com/mightyracing/events/MightyTick.java
@@ -1,16 +1,23 @@
 package com.mightyracing.events;
 
 import com.mightyracing.*;
+import com.mightyracing.config.MightyConfig;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.block.Block;
 import net.minecraft.client.resource.language.I18n;
-import net.minecraft.entity.Entity;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Map;
+import java.util.*;
 
+import static com.mightyracing.config.MightyConfig.*;
+import static java.lang.Math.round;
 
 public class MightyTick implements ServerTickEvents.EndTick{
     public static final int OFFLINE = 0;
@@ -51,6 +58,7 @@ public class MightyTick implements ServerTickEvents.EndTick{
                 sendActionTime();
             }
             case RACING -> {
+                durability();
                 sendActionTime();
             }
         }
@@ -69,8 +77,63 @@ public class MightyTick implements ServerTickEvents.EndTick{
             } else {
                 currenttime = "0:00:00";
             }
-            String actionbar = currenttime + " " + mightyplayer.interval;
+            String dur = "";
+            if (MightyRacingCommand.maxdurability != 0 && MightyRacingCommand.racingstatus == RACING ){
+                int percent = (mightyplayer.durability <= 0 ? 0 : (1 + (int) round((mightyplayer.durability / MightyRacingCommand.maxdurability) * 99)));
+                String col = percent <= 10 ? MightyRacingCommand.CRED : MightyRacingCommand.CWHITE;
+                dur = MightyRacingCommand.CGRAY + "  |  " + col + percent + "%";
+            }
+            String actionbar = currenttime + " " + mightyplayer.interval + dur;
             mightyplayer.player.sendMessageToClient(Text.literal(actionbar), true);
+        }
+    }
+    private static void durability(){
+        if (MightyRacingCommand.maxdurability == 0){
+            return;
+        }
+        Set<String> blockSet = new HashSet<>(Arrays.asList(MightyConfig.getString(SPECIAL_BLOCKS).replace(" ","").split(",")));
+        for (MightyPlayer mightyplayer : MightyPlayer.list.values()){
+            if (mightyplayer.starttime == null){
+                continue;
+            }
+
+            BlockPos pos;
+            if (mightyplayer.player.hasVehicle()){
+                pos = Objects.requireNonNull(mightyplayer.player.getVehicle()).getBlockPos().down();
+            }else{
+                pos = mightyplayer.player.getBlockPos().down();
+            }
+            World world = mightyplayer.player.getWorld();
+            Block block = world.getBlockState(pos).getBlock();
+            boolean on_block = blockSet.contains(Registries.BLOCK.getId(block).toString());
+
+            Vec3d newPos = new Vec3d(
+                    mightyplayer.player.getX(),
+                    mightyplayer.player.getY(),
+                    mightyplayer.player.getZ()
+            );
+            float speed = 0;
+            Vec3d oldPos = mightyplayer.oldPos;
+            if (mightyplayer.oldPos != null){
+                speed = (float) Math.sqrt(Math.pow(newPos.x - oldPos.x, 2) + Math.pow(newPos.y - oldPos.y, 2) + Math.pow(newPos.z - oldPos.z, 2));
+            }
+            mightyplayer.oldPos = newPos;
+            
+            if (on_block){
+                mightyplayer.durability -= Math.pow(speed * MightyConfig.getFloat(SPECIAL_MODIF),MightyConfig.getFloat(SPECIAL_POWER));
+            }else{
+                mightyplayer.durability -= Math.pow(speed * 100 * MightyConfig.getFloat(DEFAULT_MODIF),MightyConfig.getFloat(DEFAULT_POWER));
+            }
+            if (mightyplayer.durability <= 0){
+                mightyplayer.durability = 0;
+                if (MightyConfig.getBoolean(DESTROY_VEHICLE) && mightyplayer.player.hasVehicle()) {
+                    Objects.requireNonNull(mightyplayer.player.getVehicle()).kill();
+                }
+                mightyplayer.starttime = null;
+                MightyRacingCommand.raceboardPutOnlyNamecolor(mightyplayer.player.getScoreboard(),mightyplayer.player.getGameProfile().getName(),MightyRacingCommand.CRED);
+                MightyRacingCommand.broadcastToDrivers(mightyplayer.player.server,Text.translatable("info.durability.zero",Text.literal(mightyplayer.cuttedname)));
+                MightyRacingCommand.checkRaceEnd(mightyplayer.player.server);
+            }
         }
     }
 }
