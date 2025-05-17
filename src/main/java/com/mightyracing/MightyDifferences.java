7bc298a86d61d2a1d7fd2ef16f995394164fbca1
MightyBunch <timababaev@bk.ru>
2025-05-17 12:29:28 +0300

    This is new branch that supports 1.21.2-1.21.4

--- src/main/java/com/mightyracing/MightyDifferences.java
+++ src/main/java/com/mightyracing/MightyDifferences.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtFloat;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.scoreboard.*;
 import net.minecraft.scoreboard.number.NumberFormat;
 import net.minecraft.server.world.ServerWorld;
@@ -38,9 +40,22 @@ public class MightyDifferences {
         raceboardentry.setScore(score);
     }
     public static Entity getEntity(NbtCompound nbt, ServerWorld world, double xPos, double yPos, double zPos){
-        return EntityType.loadEntityWithPassengers(nbt, world, lentity -> {
+        NbtCompound transformation = nbt.getCompound("transformation");
+        NbtList scale = transformation.getList("scale",NbtList.FLOAT_TYPE);
+        transformation.put("scale", floatList(scale.getFloat(0) / 10f, scale.getFloat(1) / 10f, 1f));
+        transformation.put("translation", floatList(scale.getFloat(0) * 9f / 800f, 0f, 0f));
+        nbt.putString("text","█");
+        nbt.putByte("text_opacity",(byte) 4);
+        return EntityType.loadEntityWithPassengers(nbt, world, SpawnReason.COMMAND, lentity -> {
             lentity.refreshPositionAfterTeleport(xPos, yPos, zPos);
             return lentity;
         });
+    }
+    private static NbtList floatList(float... values) {
+        NbtList list = new NbtList();
+        for (float value : values) {
+            list.add(NbtFloat.of(value));
+        }
+        return list;
     }
 }
