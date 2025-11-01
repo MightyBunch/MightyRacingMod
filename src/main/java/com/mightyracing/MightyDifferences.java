f6a3053a9d01ab3671caa79d501580eee0537c66
MightyBunch <timababaev@bk.ru>
2025-11-01 08:37:44 +0300

    Cherrypick from 1.20.1

--- src/main/java/com/mightyracing/MightyDifferences.java
+++ src/main/java/com/mightyracing/MightyDifferences.java
@@ -2,10 +2,14 @@ package com.mightyracing;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.scoreboard.*;
 import net.minecraft.scoreboard.number.NumberFormat;
+import net.minecraft.nbt.NbtFloat;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardCriterion;
+import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 
@@ -37,10 +41,30 @@ public class MightyDifferences {
         ScoreAccess raceboardentry = scoreboard.getOrCreateScore(scoreHolder,raceboard);
         raceboardentry.setScore(score);
     }
-    public static Entity getEntity(NbtCompound nbt, ServerWorld world, double xPos, double yPos, double zPos){
+    public static Entity getEntity(ServerWorld world, double xPos, double yPos, double zPos, float scalex, float scaley, float rotationV, float rotationH, int color){
+        NbtCompound nbt = new NbtCompound();
+        nbt.putString("id", "minecraft:text_display");
+        NbtCompound transformation = new NbtCompound();
+        transformation.put("left_rotation", floatList(0f, 0f, 0f, 1f));
+        transformation.put("right_rotation", floatList(0f, 0f, 0f, 1f));
+        transformation.put("scale", floatList(scalex * 40f / 6f, scaley * 40f / 11f, 1f));
+        transformation.put("translation", floatList(scalex * 5f / 12f, 0f, 0f));
+        nbt.put("transformation", transformation);
+        nbt.putString("alignment", "center");
+        nbt.put("Rotation", floatList(rotationV, rotationH));
+        nbt.putInt("background", color);
+        nbt.putBoolean("see_through", true);
+        nbt.putString("text","{\"text\":\"\u00A0\"}");
         return EntityType.loadEntityWithPassengers(nbt, world, lentity -> {
             lentity.refreshPositionAfterTeleport(xPos, yPos, zPos);
             return lentity;
         });
+    }
+    private static NbtList floatList(float... values) {
+        NbtList list = new NbtList();
+        for (float value : values) {
+            list.add(NbtFloat.of(value));
+        }
+        return list;
     }
 }
