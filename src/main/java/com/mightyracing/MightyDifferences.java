ef4d7e517d654d244659a164cd663c1657105049
MightyBunch <timababaev@bk.ru>
2025-11-01 08:37:44 +0300

    Ok previous commit was a mistake, now i understand

--- src/main/java/com/mightyracing/MightyDifferences.java
+++ src/main/java/com/mightyracing/MightyDifferences.java
@@ -3,6 +3,8 @@ package com.mightyracing;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtFloat;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.ScoreboardCriterion;
 import net.minecraft.scoreboard.ScoreboardObjective;
@@ -35,10 +37,30 @@ public class MightyDifferences {
         ScoreboardPlayerScore raceboardentry = scoreboard.getPlayerScore(raceboardname,raceboard);
         raceboardentry.setScore(score);
     }
-    public static Entity getEntity(NbtCompound nbt, ServerWorld world, double xPos, double yPos, double zPos){
+    public static Entity getEntity(ServerWorld world, double xPos, double yPos, double zPos, float scalex, float scaley, float rotationV, float rotationH, int color){
+        NbtCompound nbt = new NbtCompound();
+        nbt.putString("id", "minecraft:text_display");
+        NbtCompound transformation = new NbtCompound();
+        transformation.put("left_rotation", floatList(0f, 0f, 0f, 1f));
+        transformation.put("right_rotation", floatList(0f, 0f, 0f, 1f));
+        transformation.put("scale", floatList(scalex * 40f / 6f, scaley * 40f / 11f, 1f));
+        transformation.put("translation", floatList(scalex * 5f / 12f, 0f, 0f));
+        nbt.put("transformation", transformation);
+        nbt.putString("alignment", "center");
+        nbt.put("Rotation", floatList(rotationV, rotationH));
+        nbt.putInt("background", color);
+        nbt.putBoolean("see_through", true);
+        nbt.putString("text","{\"text\":\"\u00A0\"}");
         return EntityType.loadEntityWithPassengers(nbt, world, lentity -> {
             lentity.refreshPositionAfterTeleport(xPos, yPos, zPos);
             return lentity;
         });
+    }
+    private static NbtList floatList(float... values) {
+        NbtList list = new NbtList();
+        for (float value : values) {
+            list.add(NbtFloat.of(value));
+        }
+        return list;
     }
 }
